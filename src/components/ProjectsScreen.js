080b72b16f0626b2d6046dccde1984723662eb4c
JJ-Dev18 <jj.murillo182020@gmail.com>
2023-01-07 17:08:32 -0500

    perpage y cant total de pages done

--- src/components/ProjectsScreen.js
+++ src/components/ProjectsScreen.js
@@ -17,7 +17,8 @@ export const ProjectsScreen = () => {
   // const { dispatch } = useContext(Context);
   const [current, setCurrent] = useState(0);
   const [projects, setprojects] = useState([]);
-
+  const [cantProjects,setCantProjects] = useState(9)
+  const [page, setPage] = useState(1)
   const { loading, data } = useFetch(`${process.env.REACT_APP_API_URL}/projects`);
 
   useEffect(() => {
@@ -30,17 +31,21 @@ export const ProjectsScreen = () => {
 
   const filterProjects = () => {
     // if (search.length === 0)
-    return projects.slice(current, current + 6);
+    return projects.slice(current, current + cantProjects);
 
     // const filtered = projects.filter((project) => project.name.includes(search));
     // return filtered.slice(currentPage, currentPage + 5);
   };
   const nextPage = () => {
-    if (projects.length > current + 6) setCurrent(current + 6);
+    if (projects.length > current + cantProjects){
+      setCurrent(current + cantProjects);
+      setPage(page + 1 )
+    }
   };
   const prevPage = () => {
     if (current > 0) {
-      setCurrent(current - 6);
+      setCurrent(current - cantProjects);
+      setPage(page - 1)
     }
   };
   // const onSearchChange = ({ target }) => {
@@ -49,28 +54,7 @@ export const ProjectsScreen = () => {
   // };
 
   return (
-    <div className="content_projects">
-      <div
-        className="content_projects-title"
-        initial={{ y: -100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ ease: "easeOut", duration: 1.5 }}
-      >
-          <Link  end="true" to={`../home`}>
-        <img src={Logo} alt="logo" />
-          </Link>
-        <h1>Projects</h1>
-      </div>
-      <div className="grid_projects">
-        {!loading ? (
-          filterProjects().map((project) => (
-            <Project key={project.nombre} project={project} />
-          ))
-        ) : (
-          <Skeleton />
-        )}
-        {/* <Skeleton /> */}
-      </div>
+    <>
       <div
         className="navigation"
         initial={{ y: -100, opacity: 0 }}
@@ -78,11 +62,40 @@ export const ProjectsScreen = () => {
         transition={{ ease: "easeOut", duration: 1.5 }}
       >
         <i className="fas fa-arrow-circle-left" onClick={prevPage}></i>
+        {!loading && (
+          <p>
+            Page {page} de {Math.ceil(data?.projects.length / cantProjects)}
+          </p>
+        )}
         <i className="fas fa-arrow-circle-right" onClick={nextPage}></i>
       </div>
-      <div className="content_projects_nav">
-        <Navbar primer="Home" segundo="About" tercero="Contact" />
+      <div className={`content_projects ${cantProjects > 6 && "scroll"}`}>
+        <div
+          className="content_projects-title"
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 1.5 }}
+        >
+          <Link end="true" to={`../home`}>
+            <img src={Logo} alt="logo" />
+          </Link>
+          <h1>Projects</h1>
+        </div>
+        <div className="grid_projects">
+          {!loading ? (
+            filterProjects().map((project) => (
+              <Project key={project.nombre} project={project} />
+            ))
+          ) : (
+            <Skeleton numProyectos={cantProjects} />
+          )}
+          {/* <Skeleton /> */}
+        </div>
+
+        <div className="content_projects_nav">
+          <Navbar primer="Home" segundo="About" tercero="Contact" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
